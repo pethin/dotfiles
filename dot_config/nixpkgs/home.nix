ff65d75df6960e888f5a5e9dab63cceb2dfaeaec
Peter Nguyen <peter@phn.sh>
2023-01-12 14:51:28 -0600

    Disable web search from spotlight

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -531,6 +531,91 @@ in
           AlertForNonmatchingDomains = false;
           ExpandPrivateAliases = true;
         };
+        "com.apple.spotlight" = {
+          # Disable web search
+          orderedItems = [
+            {
+              enabled = 1;
+              name = "APPLICATIONS";
+            }
+            {
+              enabled = 0;
+              name = "BOOKMARKS";
+            }
+            {
+              enabled = 1;
+              name = "MENU_EXPRESSION";
+            }
+            {
+              enabled = 1;
+              name = "CONTACT";
+            }
+            {
+              enabled = 1;
+              name = "MENU_CONVERSION";
+            }
+            {
+              enabled = 1;
+              name = "MENU_DEFINITION";
+            }
+            {
+              enabled = 1;
+              name = "DOCUMENTS";
+            }
+            {
+              enabled = 1;
+              name = "EVENT_TODO";
+            }
+            {
+              enabled = 1;
+              name = "DIRECTORIES";
+            }
+            {
+              enabled = 1;
+              name = "FONTS";
+            }
+            {
+              enabled = 1;
+              name = "IMAGES";
+            }
+            {
+              enabled = 1;
+              name = "MESSAGES";
+            }
+            {
+              enabled = 1;
+              name = "MOVIES";
+            }
+            {
+              enabled = 1;
+              name = "MUSIC";
+            }
+            {
+              enabled = 1;
+              name = "MENU_OTHER";
+            }
+            {
+              enabled = 1;
+              name = "PDF";
+            }
+            {
+              enabled = 1;
+              name = "PRESENTATIONS";
+            }
+            {
+              enabled = 0;
+              name = "MENU_SPOTLIGHT_SUGGESTIONS";
+            }
+            {
+              enabled = 1;
+              name = "SPREADSHEETS";
+            }
+            {
+              enabled = 1;
+              name = "SYSTEM_PREFS";
+            }
+          ];
+        };
         "com.apple.terminal" = {
           # Only use UTF-8 in Terminal.app
           StringEncodings = [4];
